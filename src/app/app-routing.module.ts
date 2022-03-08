2afa66568b8aee1967b8cb900c7a2e3f6df1675f
Rene <reipoeler.rene@gmail.com>
2022-03-08 09:40:18 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DatesComponent } from './dates/dates.component';
+import { MydatesComponent } from './mydates/mydates.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DatesComponent},
+  { path: 'MyDates', component: MydatesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

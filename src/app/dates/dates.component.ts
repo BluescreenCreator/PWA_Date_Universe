7e20b8c11e51839cffe621e85ec775ad71bbfe7b
ON20LT <lisa.trojer@web.de>
2022-03-11 11:52:36 +0100

    date category anzeige

--- src/app/dates/dates.component.ts
+++ src/app/dates/dates.component.ts
@@ -2,31 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { DateService } from '../date.service';
 import { Date } from '../date';
 
-/*
 interface DateCategory {
   value: string;
   viewValue: string;
 }
-*/
 
 @Component({
   selector: 'app-dates',
   templateUrl: './dates.component.html',
-  styleUrls: ['./dates.component.css']
+  styleUrls: ['./dates.component.css'],
 })
 export class DatesComponent implements OnInit {
   dates: Date[] = [];
 
-  /*
   dateCategory: DateCategory[] = [
-    {value: 'romatic-0', viewValue: 'Romatic'},
-    {value: 'classic-1', viewValue: 'Classic'},
-    {value: 'action-2', viewValue: 'Action'},
-    {value: 'humor-3', viewValue: 'Humor'},
+    { value: 'romatic', viewValue: 'Romatic' },
+    { value: 'classic', viewValue: 'Classic' },
+    { value: 'action', viewValue: 'Action' },
+    { value: 'humor', viewValue: 'Humor' },
   ];
-  */
- 
-  constructor(private dateService: DateService) { }
+
+  constructor(private dateService: DateService) {}
 
   ngOnInit(): void {
     this.loadDates();
@@ -36,13 +32,13 @@ export class DatesComponent implements OnInit {
     await this.dateService.add(title, category);
     await this.loadDates();
   }
-  
-  async sync(){
+
+  async sync() {
     await this.dateService.sync();
     await this.loadDates();
   }
 
-  async toggleDone(date: Date){
+  async toggleDone(date: Date) {
     await this.dateService.toggleDone(date);
     await this.loadDates();
   }
@@ -50,5 +46,4 @@ export class DatesComponent implements OnInit {
   async loadDates() {
     this.dates = await this.dateService.getAll();
   }
-
 }
